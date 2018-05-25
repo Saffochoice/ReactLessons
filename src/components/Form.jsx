48d1dab9a250c8d927cd8a9bc2276fba66b71f51
saffochoice <1choice@mail.ru>
2018-05-25 21:55:33 +0300

    Try to fix MDC

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
 
+
 function Form() {
 	return (
-		<form action="" className="signup-form mdc-theme---dark">
-			<div className="mdc-textfield"><input type="text" name="firstname" placeholder="Имя" className="mdc-textfield__input"/></div>
+		<form action="" className="signup-form --mdc-theme-on-dark">
+			<div className="mdc-text-field"><input type="text" name="firstname" placeholder="Имя" className="mdc-text-field__input "/></div>
 			
-			<div className="mdc-textfield"><input type="text" name="lastname" placeholder="Фамилия"/></div>
+			{/* <div className="mdc-textfield"><input type="text" name="lastname" placeholder="Фамилия"/></div>
 			<div className="mdc-textfield"><input type="email" name="email" placeholder="Email"/></div>
-			<div className="mdc-textfield"><input type="password" name="password" placeholder="Пароль"/></div>
-			<button className="mdc-button mdc-button--primary mdc-button--raised"></button>
+			<div className="mdc-textfield"><input type="password" name="password" placeholder="Пароль"/></div> */}
+			<button className="mdc-button mdc-button--primary mdc-button--raised">Зарегистрироватсья</button>
 		</form>
 	);
 }
